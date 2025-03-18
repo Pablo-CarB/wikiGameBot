999ae094d0d9aeef3662fa60f477464b5bc96c33
pablo <pablo.cardonab@gmail.com>
2025-03-18 12:17:03 -0400

    finished scraper for wikipedia, there are some open questions for how to treat links that link to a different wikipedia page but the link itself only references a part of that page, ie a link to the page 'car' that is called 'engine' because it links to the engine setting.

--- main.py
+++ main.py
@@ -2,26 +2,40 @@ import requests                  # sends the HTTP requests
 from bs4 import BeautifulSoup    # is what can parse HTML
 
 def get_wikipedia_links(url):
-    # Send a GET request to the Wikipedia page
     response = requests.get(url)
     
     # Check if the request was successful
     if response.status_code != 200:
-        print("Failed to retrieve the webpage.")
-        return []
+        raise ConnectionError("Failed to retrieve the webpage!")
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-    soup = soup.find_all('div',class_="mw-content-ltr mw-parser-output")[0]  # only looking at the relevant text tag
-    soup = soup.find_all('a') # only looking at links
+    soup = BeautifulSoup(response.content, "html.parser")
+    content_divs = soup.find_all("div",class_="mw-parser-output")  # only looking at the relevant text tag
+    reference_divs = soup.find_all("div", class_="reflist")           # the references section
+    further_reading_divs = soup.find_all("div", class_="refbegin")    # the further reading section
+
+
+    exclusion_list = []
+    if reference_divs is not None:
+        exclusion_list.extend(reference_divs)
+    if further_reading_divs is not None:
+        exclusion_list.extend(further_reading_divs)
+
+    for tag in exclusion_list:
+        tag.decompose()
+
+    page_links = []
+    for div in content_divs:
+        page_links.extend(div.find_all('a')) # all of the links in the main text
 
     # Find all hyperlinks
     links = []
-    for link in soup:
-        href = link.get('href')
+    for link in page_links:
+        href = link.get("href")
         # Check if the link is a Wikipedia article
-        if href and href.startswith('/wiki/') and ':' not in href:
-            full_url = 'https://en.wikipedia.org' + href
-            links.append(full_url)
+        if href and href.startswith("/wiki/") and ":" not in href:
+            full_url = "https://en.wikipedia.org" + href
+            if full_url not in links:
+                links.append(full_url)
 
     return links
 
@@ -31,14 +45,20 @@ def clean_wiki_link(link):
         partial = partial[partial.index("#")+1:]
     return partial
 
-
 def clean_up_links(links):
-    return map(clean_wiki_link , links)
+    return map(clean_wiki_link,links)
 
 
-url = 'https://en.wikipedia.org/wiki/Manifold_hypothesis' 
+def construct_dictionary(links):
+    link_term_map  = {}
+    for link in links:
+        link_term_map.update({clean_wiki_link(link):link})
+    return link_term_map
+
+
+url = "https://en.wikipedia.org/wiki/Biology" 
 wikipedia_links = get_wikipedia_links(url)
 
-# Print the results
-for link in clean_up_links(wikipedia_links):
-    print(link)
+for term in clean_up_links(wikipedia_links):
+    print(term)
+
